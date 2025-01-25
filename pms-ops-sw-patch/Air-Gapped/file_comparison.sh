2a1dc8cafb0ee48edac358da0370a4bbd0479623
nohheeyeon <heeyeon1213@gmail.com>
2025-01-25 19:34:00 +0900

    feat : 로컬 디렉토리 파일/폴더 목록 추출 기능 추가
    - 로컬 디렉토리 경로 변수 추가
    - 목록 추출 후 결과 출력

--- pms-ops-sw-patch/Air-Gapped/file_comparison.sh
+++ pms-ops-sw-patch/Air-Gapped/file_comparison.sh
@@ -6,6 +6,9 @@ PORT="PORT"
 USERNAME="USERNAME"
 REMOTE_DIRECTORY="DIRECTORY"
 
+# 로컬 디렉토리 경로
+LOCAL_DIRECTORY="로컬 디렉토리 경로"
+
 # 원격 서버의 패치셋에 있는 파일 이름 추출
 ssh -p $PORT $USERNAME@SSH_SERVER "find $REMOTE_DIRECTORY -type f -or -type d" >remote_files.txt
 if [ $? -ne 0 ]; then
@@ -13,6 +16,14 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 echo "원격 서버의 파일/폴더 목록 추출 완료"
+
+# 로컬 디렉토리의 파일/폴더 추출
+find "$LOCAL_DIRECTORY" -type f -or -type d >local_files.txt
+if [ $? -ne 0 ]; then
+    echo "로컬 디렉토리에서 파일/폴더 목록을 가져오는 데 실패"
+    exit 1
+fi
+echo "로컬 디렉토리의 파일/폴더 목록 추출 완료"
 
 # 추출된 파일/폴더 목록 출력
 echo "원격 서버의 파일/폴더 목록 :"
