6a42148a7005d0ab4b3ecbd5592f074a7bc72b7b
nohheeyeon <heeyeon1213@gmail.com>
2025-01-25 19:29:19 +0900

    feat : 원격 서버 파일/폴더 목록 추출 방식 개선
    - 변숨여을 DIRECTORY에서 REMOTE_DIRECTORY로 더 명확하게 변경
    - 'find' 명령의 출력을 'remote_files.txt' 파일로 리디렉션하여 저장
    - 'ssh' 명령이 실패할 경우 오류 메시지를 출력하고 스크립트 종료

--- pms-ops-sw-patch/Air-Gapped/file_comparison.sh
+++ pms-ops-sw-patch/Air-Gapped/file_comparison.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # SSH 서버 정보
-SSH_SERVER="SFTP_SERVER"
+SSH_SERVER="SSH_SERVER"
 PORT="PORT"
 USERNAME="USERNAME"
-DIRECTORY="DIRECTORY"
+REMOTE_DIRECTORY="DIRECTORY"
 
-# SSH를 통해 원격 명령 실행
-ssh -p $PORT $USERNAME@$SSH_SERVER "find $DIRECTORY -type f -or -type d"
-bash
+# 원격 서버의 패치셋에 있는 파일 이름 추출
+ssh -p $PORT $USERNAME@SSH_SERVER "find $REMOTE_DIRECTORY -type f -or -type d" >remote_files.txt
+if [ $? -ne 0 ]; then
+    echo "원격 서버에서 파일/폴더 목록을 가져오는 데 실패"
+    exit 1
+fi
+echo "원격 서버의 파일/폴더 목록 추출 완료"
