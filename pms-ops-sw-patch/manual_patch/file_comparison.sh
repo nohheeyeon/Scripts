761b4a286f2c1eaf966f31ca3732942dead9c938
nohheeyeon <heeyeon1213@gmail.com>
2025-01-25 19:48:54 +0900

    feat : 파일 검색 명령어의 논리 연산자 수정
    - 원격 서버와 로컬 디렉토리의 파일 검색 명령어에서 -or를 -o로 변경

--- pms-ops-sw-patch/manual_patch/file_comparison.sh
+++ pms-ops-sw-patch/manual_patch/file_comparison.sh
@@ -45,7 +45,7 @@ USERNAME="USERNAME"
 REMOTE_DIRECTORY="DIRECTORY"
 
 # 원격 서버의 패치셋에 있는 파일 이름 추출
-ssh -p $PORT $USERNAME@SSH_SERVER "find $REMOTE_DIRECTORY -type f -or -type d" >remote_files.txt
+ssh -p $PORT $USERNAME@SSH_SERVER "find $REMOTE_DIRECTORY -type f -o -type d" >remote_files.txt
 if [ $? -ne 0 ]; then
     echo "원격 서버에서 파일/폴더 목록을 가져오는 데 실패"
     exit 1
@@ -53,7 +53,7 @@ fi
 echo "원격 서버의 파일/폴더 목록 추출 완료"
 
 # 로컬 디렉토리의 파일/폴더 추출
-find "$LOCAL_DIRECTORY" -type f -or -type d >local_files.txt
+find "$LOCAL_DIRECTORY" -type f -o -type d >local_files.txt
 if [ $? -ne 0 ]; then
     echo "로컬 디렉토리에서 파일/폴더 목록을 가져오는 데 실패"
     exit 1
