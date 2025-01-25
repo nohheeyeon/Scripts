117910b00c7270555486100c30edbde01b802494
nohheeyeon <heeyeon1213@gmail.com>
2025-01-25 18:37:49 +0900

    feat : 파일 처리 로직을 분리
    - 기존에는 기존의 zip 파일 삭제와 새로운 폴더로의 이동 작업을 함께 처리
    - 변경된 코드에서는 이를 별도의 단계로 분리

--- pms-ops-sw-patch/Air-Gapped/unzip_overwrite.sh
+++ pms-ops-sw-patch/Air-Gapped/unzip_overwrite.sh
@@ -21,20 +21,22 @@ if [ -n "$(find "$target_directory" -maxdepth 1 -type f -name '*.zip')" ]; then
         unip -d "${zip_file%.zip}" "$zip_file"
         echo "$zip_file의 압축을 풀었습니다."
 
-        # 기존의 zip 파일 삭제
-        rm -r "$zip_file"
+        # patches.zip 파일 압축 해제 작업
+        patches_zip_file="$zip_file%.zip}/patches.zip"
+        if [ -f "$patches_zip_file" ]; then
+            mkdir -p "${zip_file%.zip}/patches"
+            unzip -d "${zip_file%.zip}/patches" "$patches_zip_file"
+            echo "$patches_zip_file의 패치 파일 압축을 풀었습니다."
+        fi
     done
 
-    mv "$target_directory"/*.zip "$new_folder"
-    echo "zip 파일을 '$new_folder' 폴더로 이동했습니다."
-else
-    echo "zip 파일이 존재하지 않습니다."
-fi
+    mkdir "$new_folder"
+    for folder in "$target_directory"/*/; do
+        if [ -d "$folder" ]; then
+            find "$folder" -mindepth 1 -maxdepth 1 -exec cp -rf {} "$new_folder/" \;
+            rm -r "$folder"
+            echo "$folder 내의 모든 파일과 폴더를 $new_folder/로 복사하고 원본 폴더를 삭제했습니다."
+        fi
+    done
 
-# patches.zip이 있는 지 확인하는 if 문
-if [ -e "$new_folder/patches.zip" ]; then
-    unzip -o "$new_folder/patches.zip" -d "$new_folder"
-    echo "patches.zipdmf '$new_folder' 폴더로 압축 해제했습니다."
-else
-    echo "patches.zip이 없습니다."
 fi
