dc641e01f10d4a755570a7c0ec1a17d2facf4d61
nohheeyeon <heeyeon1213@gmail.com>
2025-01-26 18:17:20 +0900

    feat : 시트 간 데이터 비교 및 행 번호 출력 기능 추가
    - 첫번째 시트의 D열 데이터를 딕셔너리로 저장
    - 네번째 시트의 E열 데이터와 비교하여 동일한 값과 첫번째 시트의 행 번호 출력 기능 추가

--- pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater_python.py
+++ pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater_python.py
@@ -8,13 +8,15 @@ wb = openpyxl.local_workbook("SW_현황_및_검증서".xlsx)
 ws1 = wb.worksheets[0]  # 첫 번째 시트
 ws4 = wb.worksheets[3]  # 네 번째 시트
 
-# 첫 번째 시트의 D열 데이터 리스트로 저장
-d_column_values = [cell.value for cell in ws1["D"]]
+# 첫 번째 시트의 D열 데이터를 딕셔너리로 저장 (값 : 행 번호)
+d_column_values = {cell.value for cell in ws1["D"] if cell.value is not None}
 
-# 네 번째 시트와 E열 데이터와 비교하여 동일한 값 찾기
+# 네번째 시트의 E열 데이터와 비교하여 동일한 값과 첫번째 시트의 행 번호 출력
 for cell in ws4["E"]:
     if cell.value in d_column_values:
-        print(f"동일한 값 발견: {cell.value}")
+        print(
+            f"동일한 값 발견: {cell.value} (첫번째 시트의 {d_column_values[cell.value]}행, 네번째 시트의 {cell.row}행)"
+        )
 
 # 엑셀 파일 닫기
 wb.close()
