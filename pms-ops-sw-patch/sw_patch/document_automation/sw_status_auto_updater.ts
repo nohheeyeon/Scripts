cad7432eab62ec8f03741ffbe396d52c032a4e05
nohheeyeon <heeyeon1213@gmail.com>
2025-01-28 17:37:02 +0900

    fix : 오타 수정
    - 'lastest_validation_sheet'를 'latest_validation_sheet'로 변수명 오타 수정

--- pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater.ts
+++ pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater.ts
@@ -60,25 +60,25 @@ function find_latest_validation_sheet(sheets: ExcelScript.Worksheet[]): ExcelScr
   // 최신 검증서의 F열(제품명) 데이터와 비교하여 동일한 값이 있는 행의 데이터를 SW현황으로 복사
   function update_sw_summary_sheet(
     sw_summary_sheet: ExcelScript.Worksheet,
-    lastest_validation_sheet: ExcelScript.Worksheet,
+    latest_validation_sheet: ExcelScript.Worksheet,
     sw_patch_names: { [key: string]: number },
     validation_patch_names: { [key: string]: number }
   ) {
     Object.keys(validation_patch_names).forEach(patch_name_values => {
       if (sw_patch_names[patch_name_values] !== undefined) {
         let row_in_sw_summary_sheet: number = sw_patch_names[patch_name_values];
-        let row_in_lastest_validation_sheet: number = validation_patch_names[patch_name_values];
+        let row_in_latest_validation_sheet: number = validation_patch_names[patch_name_values];
   
         // 해당하는 셀 복사 작업
-        sw_summary_sheet.getCell(row_in_sw_summary_sheet - 1, 5).setValue(lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 3).getValue()); // D열(발표일) 값 복사
-        sw_summary_sheet.getCell(row_in_sw_summary_sheet - 1, 6).setValue(lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 8).getValue()); // H열(버전) 값 복사
-        sw_summary_sheet.getCell(row_in_sw_summary_sheet - 1, 7).setValue(lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 9).getValue()); // I열(패치 파일명) 값 복사
+        sw_summary_sheet.getCell(row_in_sw_summary_sheet - 1, 5).setValue(latest_validation_sheet.getCell(row_in_latest_validation_sheet - 1, 3).getValue()); // D열(발표일) 값 복사
+        sw_summary_sheet.getCell(row_in_sw_summary_sheet - 1, 6).setValue(latest_validation_sheet.getCell(row_in_latest_validation_sheet - 1, 8).getValue()); // H열(버전) 값 복사
+        sw_summary_sheet.getCell(row_in_sw_summary_sheet - 1, 7).setValue(latest_validation_sheet.getCell(row_in_latest_validation_sheet - 1, 9).getValue()); // I열(패치 파일명) 값 복사
   
         // 로그 출력
-        console.log(`동일한 값 발견: ${patch_name_values} (SW현황의 ${row_in_sw_summary_sheet}행, 최신 검증서의 ${row_in_lastest_validation_sheet}행)`);
-        console.log(`최신 검증서의 D열 값 '${lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 3).getValue()}'을 SW현황의 F열로 복사`);
-        console.log(`최신 검증서의 H열 값 '${lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 8).getValue()}'을 SW현황의 G열로 복사`);
-        console.log(`최신 검증서의 I열 값 '${lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 9).getValue()}'을 SW현황의 H열로 복사`);
+        console.log(`동일한 값 발견: ${patch_name_values} (SW현황의 ${row_in_sw_summary_sheet}행, 최신 검증서의 ${row_in_latest_validation_sheet}행)`);
+        console.log(`최신 검증서의 D열 값 '${latest_validation_sheet.getCell(row_in_latest_validation_sheet - 1, 3).getValue()}'을 SW현황의 F열로 복사`);
+        console.log(`최신 검증서의 H열 값 '${latest_validation_sheet.getCell(row_in_latest_validation_sheet - 1, 8).getValue()}'을 SW현황의 G열로 복사`);
+        console.log(`최신 검증서의 I열 값 '${latest_validation_sheet.getCell(row_in_latest_validation_sheet - 1, 9).getValue()}'을 SW현황의 H열로 복사`);
       }
     });
   }
