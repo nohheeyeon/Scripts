280a7c9cc3678df3ddbc4386c02cf05085e68aa6
nohheeyeon <heeyeon1213@gmail.com>
2025-01-28 14:24:01 +0900

    feat : 중복 코드를 함수로 분리
    - 복사 작업 로그를 출력하는 log_copy_operation 함수 추가

--- pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater_python.py
+++ pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater_python.py
@@ -1,10 +1,22 @@
 import openpyxl
+import openpyxl.utils
 import pandas as pd
 
 
 def copy_cell_value(src_ws, src_row, src_col, dest_ws, dest_row, dest_col):
     value = src_ws.cell(row=src_row, column=src_col).value
     dest_ws.cell(row=dest_row, column=dest_col, value=value)
+
+
+def log_copy_operation(
+    cell_value, row_in_ws1, row_in_ws4, src_ws, src_row, src_col, target_col
+):
+    print(
+        f"동일한 값 발견: {cell_value} (첫번째 시트의 {row_in_ws1}행, 네번째 시트의 {row_in_ws4}헹)"
+    )
+    print(
+        f"네번째 시트의 {openpyxl.utils.get_column_letter(src_col)}열 값 '{src_ws.cell(row=src_row, column=src_col).value}'을 첫번째 시트의 {openpyxl.utils.get_column_letter(target_col)}열로 복사"
+    )
 
 
 # 작업할 엑셀 시트 불러오기
@@ -26,22 +38,11 @@ for cell in ws4["E"]:
 
         # 복사 작업 수행
         copy_cell_value(ws4, row_in_ws4, 4, ws1, row_in_ws1, 6)  # D열 값 복사
+        log_copy_operation(cell.value, row_in_ws1, row_in_ws4, ws4, row_in_ws4, 4, 6)
         copy_cell_value(ws4, row_in_ws4, 8, ws1, row_in_ws1, 7)  # H열 값 복사
+        log_copy_operation(cell.value, row_in_ws1, row_in_ws4, ws4, row_in_ws4, 8, 7)
         copy_cell_value(ws4, row_in_ws4, 9, ws1, row_in_ws1, 8)  # I열 값 복사
-
-        print(
-            f"동일한 값 발견: {cell.value} 첫 번째 시트의 {row_in_ws4}행, 네번째 시트이 {row_in_ws4}헹"
-        )
-        print(
-            f"네번째 시트의 D열 값 '{ws4.cell(row=row_in_ws4, column=4).value}'을 첫번째 시트의 F열로 복사"
-        )
-        print(
-            f"네번째 시트의 H열 값 '{ws4.cell(row=row_in_ws4, column=8).value}'을 첫번째 시트의 G열로 복사"
-        )
-        print(
-            f"네번째 시트의 I열 값 '{ws4.cell(row=row_in_ws4, column=9).value}'을 첫번째 시트의 H열로 복사"
-        )
-
+        log_copy_operation(cell.value, row_in_ws1, row_in_ws4, ws4, row_in_ws4, 9, 8)
 # 엑셀 파일 저장 및 닫기
 wb.save("SW_현황_및_검증서.xlsx")
 wb.close()
