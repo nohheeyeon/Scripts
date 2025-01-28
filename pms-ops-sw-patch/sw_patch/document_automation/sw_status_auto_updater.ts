f3fec06a393ef67566ac41dd727fb3b25e81e689
nohheeyeon <heeyeon1213@gmail.com>
2025-01-28 17:23:24 +0900

    feat : 최신 검증서 접근 시트 및 열 인덱스 수정
    - 'OS 분리 배포 현황' 시트 추가에 따른 접근 시트 변경
    - 시트 내의 'OS 배포 타입' 열 추가에 따른 열 인덱스 수정

--- pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater.ts
+++ pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater.ts
@@ -4,13 +4,13 @@ function main(workbook: ExcelScript.Workbook) {
   
     // SW현황과 최신 검증서 가져오기
     let sw_summary_sheet = sheets[0];  // SW현황
-    let lastest_validation_sheet = sheets[3];  // 최신 검증서
+    let lastest_validation_sheet = sheets[4];  // 최신 검증서
     
     // SW현황의 제품명 데이터를 가져오기
     let sw_patch_names = get_patch_names(sw_summary_sheet, 'D');
   
     // 최신 검증서의 제품명 데이터를 가져오기
-    let validation_patch_names = get_patch_names(lastest_validation_sheet, 'E');
+    let validation_patch_names = get_patch_names(lastest_validation_sheet, 'F');
     
     // 제품명을 비교하여 데이터를 업데이트
     update_sw_summary_sheet(sw_summary_sheet, lastest_validation_sheet, sw_patch_names, validation_patch_names);
@@ -32,7 +32,7 @@ function main(workbook: ExcelScript.Workbook) {
     return patch_name_column_values;
   }
   
-  // 최신 검증서의 E열(제품명) 데이터와 비교하여 동일한 값이 있는 행의 데이터를 SW현황으로 복사
+  // 최신 검증서의 F열(제품명) 데이터와 비교하여 동일한 값이 있는 행의 데이터를 SW현황으로 복사
   function update_sw_summary_sheet(
     sw_summary_sheet: ExcelScript.Worksheet,
     lastest_validation_sheet: ExcelScript.Worksheet,
@@ -46,14 +46,14 @@ function main(workbook: ExcelScript.Workbook) {
   
         // 해당하는 셀 복사 작업
         sw_summary_sheet.getCell(row_in_sw_summary_sheet - 1, 5).setValue(lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 3).getValue()); // D열(발표일) 값 복사
-        sw_summary_sheet.getCell(row_in_sw_summary_sheet - 1, 6).setValue(lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 7).getValue()); // H열(버전) 값 복사
-        sw_summary_sheet.getCell(row_in_sw_summary_sheet - 1, 7).setValue(lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 8).getValue()); // I열(패치 파일명) 값 복사
+        sw_summary_sheet.getCell(row_in_sw_summary_sheet - 1, 6).setValue(lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 8).getValue()); // H열(버전) 값 복사
+        sw_summary_sheet.getCell(row_in_sw_summary_sheet - 1, 7).setValue(lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 9).getValue()); // I열(패치 파일명) 값 복사
   
         // 로그 출력
         console.log(`동일한 값 발견: ${patch_name_values} (SW현황의 ${row_in_sw_summary_sheet}행, 최신 검증서의 ${row_in_lastest_validation_sheet}행)`);
         console.log(`최신 검증서의 D열 값 '${lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 3).getValue()}'을 SW현황의 F열로 복사`);
-        console.log(`최신 검증서의 H열 값 '${lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 7).getValue()}'을 SW현황의 G열로 복사`);
-        console.log(`최신 검증서의 I열 값 '${lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 8).getValue()}'을 SW현황의 H열로 복사`);
+        console.log(`최신 검증서의 H열 값 '${lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 8).getValue()}'을 SW현황의 G열로 복사`);
+        console.log(`최신 검증서의 I열 값 '${lastest_validation_sheet.getCell(row_in_lastest_validation_sheet - 1, 9).getValue()}'을 SW현황의 H열로 복사`);
       }
     });
   }
