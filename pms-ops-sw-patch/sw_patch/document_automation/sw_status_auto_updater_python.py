14572c90ab840b950adb18ea4be9f0724652e639
nohheeyeon <heeyeon1213@gmail.com>
2025-01-28 14:18:03 +0900

    feat : 중복 코드를 함수로 분리
    - 네번째 시트의 셀 값을 첫번째 시트로 복사하는 copy_cell_value 함수 추가

--- pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater_python.py
+++ pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater_python.py
@@ -1,6 +1,12 @@
 import openpyxl
 import pandas as pd
 
+
+def copy_cell_value(src_ws, src_row, src_col, dest_ws, dest_row, dest_col):
+    value = src_ws.cell(row=src_row, column=src_col).value
+    dest_ws.cell(row=dest_row, column=dest_col, value=value)
+
+
 # 작업할 엑셀 시트 불러오기
 wb = openpyxl.local_workbook("SW_현황_및_검증서".xlsx)
 
@@ -11,21 +17,18 @@ ws4 = wb.worksheets[3]  # 네 번째 시트
 # 첫 번째 시트의 D열 데이터를 딕셔너리로 저장 (값 : 행 번호)
 d_column_values = {cell.value for cell in ws1["D"] if cell.value is not None}
 
-# 네번째 시트의 E열 데이터와 비교하여 동일한 값이 있는 행의 D열 데이터를 첫번째 시트의 F열(발표일)로 복사
+# 네번째 시트의 E열 데이터와 비교하여 동일한 값이 있는 행의 데이터를 첫번째 시트로 복사
 for cell in ws4["E"]:
     if cell.value in d_column_values:
         # 동일한 값이 있는 행 번호
         row_in_ws1 = d_column_values[cell.value]
         row_in_ws4 = cell.row
 
-        # 네번째 시트의 D열 값을 첫번째 시트의 F열로 복사
-        ws1.cell(row=row_in_ws1, column=6, value=ws4.cell(row_in_ws4, column=4).value)
-        ws1.cell(
-            row=row_in_ws1, column=7, value=ws4.cell(row=row_in_ws4, column=8).value
-        )
-        ws1.cell(
-            row=row_in_ws4, column=8, value=ws4.cell(row=row_in_ws4, column=9).value
-        )
+        # 복사 작업 수행
+        copy_cell_value(ws4, row_in_ws4, 4, ws1, row_in_ws1, 6)  # D열 값 복사
+        copy_cell_value(ws4, row_in_ws4, 8, ws1, row_in_ws1, 7)  # H열 값 복사
+        copy_cell_value(ws4, row_in_ws4, 9, ws1, row_in_ws1, 8)  # I열 값 복사
+
         print(
             f"동일한 값 발견: {cell.value} 첫 번째 시트의 {row_in_ws4}행, 네번째 시트이 {row_in_ws4}헹"
         )
