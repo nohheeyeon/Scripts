39bbe3032d0a5d89aa5f86ca828b2cc7eb5c7ca9
nohheeyeon <heeyeon1213@gmail.com>
2025-02-09 17:26:33 +0900

    feat : 독립실행패치 파일 탐색 기능 추가
    - 독립실행패치파일이 특정 폴더에 존재하는 지 확인할 필요성이 있음
    - os.walk를 사용해 폴더 내에서 파일명을 탐색하는 기능 추가

--- pms-ops-tacs-patch-verification/standalone_patch_verification/standalone_patch_auto_install_X86.py
+++ pms-ops-tacs-patch-verification/standalone_patch_verification/standalone_patch_auto_install_X86.py
@@ -7,3 +7,14 @@ sw_patch_list = os.path.join(desktop, "sw_patch_list.xlsx")
 ms_patch_list = os.path.join(desktop, "ms_patch_list.xlsx")
 
 patch_folder = os.path.join(user_home, r"AppData\Local\Temp\package\standalone")
+
+
+def find_file_in_folder(base_folder, target_name):
+    for root, dirs, files in os.walk(base_folder):
+        for file in files:
+            if (
+                file.lower() == target_name.lower()
+                or file.lower() == f"{target_name.lower()}.exe"
+            ):
+                return os.path.join(root, file)
+    return None
