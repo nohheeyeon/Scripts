a85d0c16ea1fdf6841f122b5c8ee6efab10625af
nohheeyeon <heeyeon1213@gmail.com>
2025-02-09 17:57:42 +0900

    feat : PatchType Enum으로 패치 유형 구분
    - 기존 코드에서 bool admin으로 sw와 ms 패치를 구분했지만, 의미가 명확하지 않아 가독성이 저하
    - PatchType이라는 Enum을 도입하여 sw와 ms 패치를 구분
    - bool admin 제거
    - Process_patch_list 메서드에서 PatchType을 매개변수로 받아 설치 유형을 결정하도록 수정

--- pms-ops-tacs-patch-verification/standalone_patch_verification/standalone_patch_auto_install.py
+++ pms-ops-tacs-patch-verification/standalone_patch_verification/standalone_patch_auto_install.py
@@ -2,17 +2,23 @@ import os
 import subprocess
 import sys
 import time
+from enum import Enum
 
 import pandas as pd
 
 # 실행 순서
-# 1. 패치셋의 standalone 폴더를 각 VM의 "C:\Users\사용자이름\AppData\Local\Temp\package" 경로로 가져오기
+# 1. 패치셋의 standalone 폴더를 각 VM의 "C:\Users\사용자이름\AppData\Local\Temp\package" 경로로 가져오기 (/package 폴더는 새롭게 생성해야 합니다.)
 # 2. standalone_patch_auto_install.py의 sw_patch_list_file와 ms_patch_list_file를 이 달에 해당하는 패치 목록의 이름으로 변경하기
 # 3. standalone_patch_auto_install_X86.bat 또는 standalone_patch_auto_install_AMD64.bat를 관리자 권한으로 실행
 # 4. 스크립트 실행 종료 후 업데이트 확인
 #   - SW 패치 : "제어판\프로그램\프로그램 및 기능\프로그램 제거 또는 변경"에서 업데이트가 설치되었는지 확인
 #   - MS 패치 : "제어판\프로그램\프로그램 및 기능\설치된 업데이트"에서 KB ID가 설치되었는지 확인
 #                   또한 "C:\Users\사용자이름\AppData\Local\Temp\package" 위치에 해당 ID 값의 폴더가 생성되었는지 확인
+
+
+class PatchType(Enum):
+    SW = "SW"
+    MS = "MS"
 
 
 class StandAlonePatchInstaller:
@@ -70,7 +76,7 @@ class StandAlonePatchInstaller:
             print(f"관리자 실행 중 오류 발생: {e}")
             return False
 
-    def process_patch_list(self, excel_file, column_name, keyword, admin=False):
+    def process_patch_list(self, excel_file, column_name, keyword, patch_type):
         try:
             df = pd.read_excel(excel_file, engine="openpyxl")
             filtered_data = df[df[column_name].str.contains(keyword, na=False)]
@@ -83,9 +89,9 @@ class StandAlonePatchInstaller:
                     )
                     if patch_file_path:
                         print(f"패치 파일 경로: {patch_file_path}")
-                        if admin:
+                        if patch_type == PatchType.SW:
                             success = self.install_sw_patch(patch_file_path)
-                        else:
+                        elif patch_type == PatchType.MS:
                             success = self.install_ms_patch(patch_file_path)
                         if not success:
                             print(f"{patch_file_name} 처리 실패, 다음 패치로 이동")
@@ -99,12 +105,16 @@ class StandAlonePatchInstaller:
     def process_sw_patch_list(self):
         print("SW 패치 리스트 처리 중")
         keyword = "X86" if self.target_architecture == "X86" else "AMD64"
-        self.process_patch_list(self.sw_patch_list_file, "비트", keyword, admin=True)
+        self.process_patch_list(
+            self.sw_patch_list_file, "비트", keyword, patch_type=PatchType.SW
+        )
 
     def process_ms_patch_list(self):
         print("MS 패치 리스트 처리 중")
         keyword = "32비트" if self.target_architecture == "X86" else "64비트"
-        self.process_patch_list(self.ms_patch_list_file, "제목", keyword, admin=False)
+        self.process_patch_list(
+            self.ms_patch_list_file, "제목", keyword, patch_type=PatchType.MS
+        )
 
 
 if __name__ == "__main__":
