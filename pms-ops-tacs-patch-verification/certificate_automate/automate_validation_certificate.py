81c38594d85fac9ecc043a25ab5c57ecf7d50249
nohheeyeon <heeyeon1213@gmail.com>
2025-02-09 19:15:34 +0900

    feat : .docx에 ms 패치의 Kb id 및 sw 패치 버전 정보 추가
    - 21H2 및 22H2 패치의 개수와 KB ID 정보를 추가
    - office 2016의 개수 및 KB ID 추가
    - SW 패치의 버전 정보 추가

--- pms-ops-tacs-patch-verification/certificate_automate/automate_validation_certificate.py
+++ pms-ops-tacs-patch-verification/certificate_automate/automate_validation_certificate.py
@@ -28,8 +28,8 @@ def get_unique_subfolder(base_path):
         raise Exception("data 폴더 하위에 폴더가 하나만 있어야 합니다.")
 
 
-def map_patch_titles(file_list, excel_file):
-    df = pd.read_excel(excel_file)
+def map_patch_titles(file_list, ms_excel_file):
+    df = pd.read_excel(ms_excel_file)
     patch_title_map = dict(zip(df["패치파일"], df["제목"]))
     mapped_files = []
     for file in file_list:
@@ -41,17 +41,17 @@ def map_patch_titles(file_list, excel_file):
     return mapped_files
 
 
-def count_21h2_titles(excel_file):
-    df = pd.read_excel(excel_file)
+def count_21h2_titles(ms_excel_file):
+    df = pd.read_excel(ms_excel_file)
     return df["제목"].str.contains("21H2").sum()
 
 
-def count_22h2_titles(excel_file):
-    df = pd.read_excel(excel_file)
+def count_22h2_titles(ms_excel_file):
+    df = pd.read_excel(ms_excel_file)
     return df["제목"].str.contains("22H2").sum()
 
 
-def list_files_in_v1(v1_folder_path, excel_file):
+def list_files_in_v1(v1_folder_path, ms_excel_file):
     if not os.path.exists(v1_folder_path) or not os.path.isdir(v1_folder_path):
         return "v1 폴더가 존재하지 않습니다."
 
@@ -62,7 +62,6 @@ def list_files_in_v1(v1_folder_path, excel_file):
     for root, dirs, files in os.walk(v1_folder_path):
         if "sw_files" in dirs:
             dirs.remove("sw_files")
-
         relative_path = os.path.relpath(root, v1_folder_path).replace(os.sep, "/")
         filtered_files = [
             file
@@ -94,7 +93,7 @@ def list_files_in_v1(v1_folder_path, excel_file):
         for base_dir, grouped_files in sw_files_grouped.items():
             section_count += 1
             prefix = chr(96 + section_count) + "."
-            mapped_files = map_patch_titles(grouped_files, excel_file)
+            mapped_files = map_patch_titles(grouped_files, ms_excel_file)
             file_list = [f"{i + 1}) {file}" for i, file in enumerate(mapped_files)]
             section = (
                 f"{prefix} sw_files/{base_dir} 하위 {len(grouped_files)}개 파일 확인\n"
@@ -103,6 +102,47 @@ def list_files_in_v1(v1_folder_path, excel_file):
             result.append(section)
 
     return "\n\n".join(result)
+
+
+def get_kb_ids_grouped(ms_excel_file):
+    df = pd.read_excel(ms_excel_file)
+
+    # 중복 제거 추가
+    kb_21h2 = df[df["제목"].str.contains("21H2", na=False)]["KB ID"].dropna().unique()
+    kb_22h2 = df[df["제목"].str.contains("22H2", na=False)]["KB ID"].dropna().unique()
+
+    # KB 접두어 추가 + 중복 제거
+    kb_21h2 = sorted(set(f"KB{str(kb_id)}" for kb_id in kb_21h2))
+    kb_22h2 = sorted(set(f"KB{str(kb_id)}" for kb_id in kb_22h2))
+
+    return ", ".join(kb_21h2), ", ".join(kb_22h2)
+
+
+def get_office_kb_ids(ms_excel_file):
+    df = pd.read_excel(ms_excel_file)
+
+    office_kb_ids = (
+        df[df["제목"].str.contains("보안 업데이트", na=False)]["KB ID"]
+        .dropna()
+        .unique()
+    )
+
+    office_kb_ids = sorted(set(f"KB{str(kb_id)}" for kb_id in office_kb_ids))
+
+    return len(office_kb_ids), ", ".join(office_kb_ids)
+
+
+def get_software_versions(ms_excel_file, software_names):
+    df = pd.read_excel(ms_excel_file)
+
+    versions = {}
+    for software in software_names:
+        matched_versions = (
+            df[df["패치명"].str.contains(software, na=False)]["버전"].dropna().unique()
+        )
+        versions[software] = ", ".join(sorted(set(matched_versions)))
+
+    return versions
 
 
 def set_font_size(document, size):
@@ -123,9 +163,15 @@ def update_docx_with_content(source_file_path, content_to_add, new_file_path, mo
         data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
         folder_name = get_unique_subfolder(data_dir)
         current_year = datetime.now().year
-        excel_file = os.path.join(data_dir, "ms_patch_list.xlsx")
-        count_21h2 = count_21h2_titles(excel_file)
-        count_22h2 = count_22h2_titles(excel_file)
+        ms_excel_file = os.path.join(data_dir, "ms_patch_list.xlsx")
+        sw_excel_file = os.path.join(data_dir, "sw_patch_list.xlsx")
+        count_21h2 = count_21h2_titles(ms_excel_file)
+        count_22h2 = count_22h2_titles(ms_excel_file)
+        kb_ids_21h2, kb_ids_22h2 = get_kb_ids_grouped(ms_excel_file)
+        office_count, office_kb_ids = get_office_kb_ids(ms_excel_file)
+
+        software_list = ["Adobe Acrobat Reader", "BandiZip", "Chrome"]
+        software_versions = get_software_versions(sw_excel_file, software_list)
 
         table = document.tables[1]
         for row in table.rows:
@@ -141,31 +187,42 @@ def update_docx_with_content(source_file_path, content_to_add, new_file_path, mo
                     cell.text = cell.text.replace(
                         "월 증분 패치", f"{month}월 증분 패치"
                     )
-        target_numbers = {"4", "6", "7", "8", "9"}
-        for row in table.rows:
-            if row.cells[0].text.strip() in target_numbers:
-                for cell in row.cells:
-                    if row.cells[0].text.strip() == "9":
-                        cell.text = re.sub(r"(?<!\d)년", f"{current_year}년", cell.text)
-                        cell.text = re.sub(r"(?<!\d)월", f"{month}월", cell.text)
-                        cell.text = re.sub(
-                            r"21H2\(.*?\)", f"21H2({count_21h2})", cell.text
-                        )
-                        cell.text = re.sub(
-                            r"22H2\(.*?\)", f"22H2({count_22h2})", cell.text
-                        )
-                    if "확인 사항" in cell.text:
-                        if content_to_add not in cell.text:
-                            cell.text = (
-                                cell.text.split("확인 사항")[0]
-                                + "확인 사항\n"
-                                + content_to_add
-                                + "\n"
-                                + "\n".join(cell.text.split("확인 사항")[1:])
-                            )
+            target_numbers = {"4", "6", "7", "8", "9"}
+            for row in table.rows:
+                if row.cells[0].text.strip() in target_numbers:
+                    for cell in row.cells:
+                        if row.cells[0].text.strip() == "9":
+                            header_text = f"하위 서버 PMS 클라이언트> 최신 패치 탭에서 발표일 {current_year}년 {month}월까지 노출 확인\n\n"
+                            if len(row.cells) > 1:
+                                right_cell = row.cells[1]
+                                content = right_cell.text if right_cell.text else ""
+
+                                content = (
+                                    f"21H2({count_21h2}) - {kb_ids_21h2}\n"
+                                    f"22H2({count_22h2}) - {kb_ids_22h2}\n\n"
+                                    f"Office 2016({office_count}) - {office_kb_ids}\n\n"
+                                )
+
+                                for software in software_list:
+                                    version = software_versions.get(
+                                        software, "버전 정보 없음"
+                                    )
+                                    content += f"{software} - {version}\n"
+
+                                right_cell.text = header_text + content
+                        if "확인 사항" in cell.text:
+                            if content_to_add not in cell.text:
+                                cell.text = (
+                                    cell.text.split("확인 사항")[0]
+                                    + "확인 사항\n"
+                                    + content_to_add
+                                    + "\n"
+                                    + "\n".join(cell.text.split("확인 사항")[1:])
+                                )
         set_font_size(document, 10)
         document.save(new_file_path)
         print(f"파일이 성공적으로 생성되었습니다: {new_file_path}")
+
     except Exception as e:
         print(f"오류 발생: {e}")
 
