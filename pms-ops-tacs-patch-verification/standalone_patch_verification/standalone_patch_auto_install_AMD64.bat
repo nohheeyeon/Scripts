a284f62ed28198d5cb025631adb7fff3b20b1148
nohheeyeon <heeyeon1213@gmail.com>
2025-02-09 17:53:38 +0900

    refactor : 배치 파일 수정
    - 86비트 및 64비트 용 배치 파일에서 통합된 Pytho 스크립트를 호출하도록 수정

--- pms-ops-tacs-patch-verification/standalone_patch_verification/standalone_patch_auto_install_AMD64.bat
+++ pms-ops-tacs-patch-verification/standalone_patch_verification/standalone_patch_auto_install_AMD64.bat
@@ -1,4 +1,4 @@
 @echo off
 cd %userprofile%\Desktop
-python standalone_patch_auto_install_X86.py
+python standalone_patch_auto_install.py AMD64
 pause
---
+++ pms-ops-tacs-patch-verification/standalone_patch_verification/standalone_patch_auto_install_X86.bat
@@ -0,0 +1,4 @@
+@echo off
+cd %userprofile%\Desktop
+python standalone_patch_auto_install.py X86
+pause
