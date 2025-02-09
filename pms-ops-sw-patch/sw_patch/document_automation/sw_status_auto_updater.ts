1d3addb9b4f52880498922e5cf360bb2e323e0f8
nohheeyeon <heeyeon1213@gmail.com>
2025-02-09 13:40:59 +0900

    feat : sw현황 시트에 버전 데이터 복사
    - 최신 검증서 시트의 버전 데이터를 sw현황 시트에 업데이트하는 기능 필요
    - 공통된 제품명을 기준으로 최신 검증서 시트의 버전 데이터를 sw현황 시트에 자동으로 복사하는 기능 추가

--- pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater.ts
+++ pms-ops-sw-patch/sw_patch/document_automation/sw_status_auto_updater.ts
@@ -1,5 +1,13 @@
+////////////////////////////////////////////////////////////////////////////////////
+// "PMS SW 패치 현황 및 검증서"를 최신화시키는 과정에서 사용
+
+// 코드 실행 과정 :
+// 1. "PMS SW 패치 현황 및 검증서" 접근
+// 2. 첫 번째 시트(SW현황)과 정규표현식으로 찾은 최신 검증서 시트의 제품명을 비교
+// 3. 동일한 제품명이 있다면, 최신 검증서 시트의 데이터를 SW현황 시트로 복사(복사 항목 : 발표일, 버전, 패치 파일명)
+////////////////////////////////////////////////////////////////////////////////////
 function main(workbook: ExcelScript.Workbook) {
-    // "SW 패치 현황 및 검증서" 불러오기
+  // "PMS SW 패치 현황 및 검증서" 불러오기
   let sheets = workbook.getWorksheets();
 
   // SW현황 시트를 가져오기
@@ -27,7 +35,7 @@ function main(workbook: ExcelScript.Workbook) {
   let validation_release_date_cell_address = find_cell_with_regex(latest_validation_sheet, /^발표일$/);
 
   // "버전" 셀 찾기
-  let sw_version_cell_address = find_cell_with_regex(sw_summary_sheet, /^버전$/);
+  let sw_version_cell_address = find_cell_with_regex(sw_summary_sheet, /^버전\(G\)$/);
   let validation_version_cell_address = find_cell_with_regex(latest_validation_sheet, /^버전$/);
 
   // "패치 파일(H)"/"패치 파일" 셀 찾기
@@ -54,7 +62,7 @@ function main(workbook: ExcelScript.Workbook) {
     let validation_release_dates = get_column_data(latest_validation_sheet, validation_release_date_cell_address.row + 1, validation_release_date_cell_address.column);
     
     // "버전" 셀 하위의 모든 열 데이터를 추출
-    let validation_versions = get_column_data(latest_validation_sheet, validation_version_cell_address.row+1, validation_version_cell_address.column);
+    let validation_versions = get_column_data(latest_validation_sheet, validation_version_cell_address.row + 1, validation_version_cell_address.column);
    
     // "패치 파일(H)" 및 "패치 파일" 셀 하위의 모든 열 데이터를 추출
     let validation_patch_file = get_column_data(latest_validation_sheet, validation_patch_file_cell_address.row + 1, validation_patch_file_cell_address.column);
@@ -69,33 +77,39 @@ function main(workbook: ExcelScript.Workbook) {
   // 공통된 제품명 찾기
   let common_values = find_common_values(sw_product_names, validation_product_names);
   console.log("공통된 제품명:", common_values);
+
 // 공통된 제품명과 같은 행에 있는 발표일 데이터를 SW현황 시트에 복사
 for (let product of common_values) {
   let product_index_in_sw = sw_product_names.indexOf(product);
   let product_index_in_validation = validation_product_names.indexOf(product);
 
   if (product_index_in_validation !== -1 && product_index_in_sw !== -1) {
+
     let validation_release_date = validation_release_dates[product_index_in_validation];
-    let validation_patch_files = validation_patch_file[product_index_in_validation];
     let sw_release_date_cell = sw_summary_sheet.getCell(sw_release_date_cell_address.row + product_index_in_sw, sw_release_date_cell_address.column);
-    
+    let validation_patch_files = validation_patch_file[product_index_in_validation];
     let sw_patch_file_cell = sw_summary_sheet.getCell(sw_patch_file_cell_address.row + product_index_in_sw, sw_patch_file_cell_address.column);
+    let validation_product_version = validation_versions[product_index_in_validation];
+    let sw_product_version_cell = sw_summary_sheet.getCell(sw_version_cell_address.row + product_index_in_sw, sw_version_cell_address.column);
+    
     // 발표일 데이터를 SW현황 시트에 복사
     sw_release_date_cell.setValue(validation_release_date);
 
     sw_patch_file_cell.setValue(validation_patch_files);
 
+    sw_product_version_cell.setValue(validation_product_version);
+
     // 복사가 완료되었음을 로그로 출력
     console.log(`제품명: ${product}, 최신 검증서 발표일: ${validation_release_date}, SW현황 시트에 복사 완료`);
 
     console.log(`제품명: ${product}, 최신 검증서 패치 파일: ${validation_patch_files}, SW현황 시트에 복사 완료`);
+
+    console.log(`제품명: ${product}, 최신 검증서 버전: ${validation_product_version}, SW현황 시트에 복사 완료`);
   }
 }
 } else {
   console.log("SW현황 시트 또는 최신 검증서 시트에서 '제품' 셀을 찾을 수 없습니다.");
 }
-
-
 
 }
 
