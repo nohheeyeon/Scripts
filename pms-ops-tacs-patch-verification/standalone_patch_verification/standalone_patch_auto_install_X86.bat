6c6c871edc4430672d3f7cacbeb3fc4dcf71d35a
nohheeyeon <heeyeon1213@gmail.com>
2025-02-09 18:19:44 +0900

    feat : 배치 파일에 가상 환경 활성화 기능 추가 및 .gitignore 업데이트
    - 기존 배치 파일이 Python 가상 환경을 활용하지 않아 의존성 충돌 가능성이 있음
    - 배치 파일에 가상 환경 활성화를 추가
    - 상대 경로 기반으로 Python 스크립트를 실행하도록 수정
    - .gitignore에 .venv/를 추가하여 가상 환경 디렉토리를 git에서 제외

--- pms-ops-tacs-patch-verification/standalone_patch_verification/standalone_patch_auto_install_X86.bat
+++ pms-ops-tacs-patch-verification/standalone_patch_verification/standalone_patch_auto_install_X86.bat
@@ -1,4 +1,12 @@
 @echo off
-cd %userprofile%\Desktop
-python standalone_patch_auto_install.py X86
+
+REM 가상 환경 활성화
+call %~dp0\.venv\Scripts\activate.bat
+
+REM 스크립트 실행
+python %~dp0\standalone_patch_auto_install.py X86
+
+REM 가상 환경 비활성화
+deactivate
+
 pause
