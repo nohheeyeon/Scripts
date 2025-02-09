94ed98f7c28a66893809893a01baaec043f2f4ee
nohheeyeon <heeyeon1213@gmail.com>
2025-02-09 17:27:51 +0900

    feat : silent 설치 기능 추가
    - 패치 설치 시 사용자 입력을 최소화할 필요가 있음
    - /quiet 및 /norestart 옶변을 사용하여 패치 설치 중 불필요한 사용자 입력 제거

--- pms-ops-tacs-patch-verification/standalone_patch_verification/standalone_patch_auto_install_AMD64.py
+++ pms-ops-tacs-patch-verification/standalone_patch_verification/standalone_patch_auto_install_AMD64.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 user_home = os.path.expanduser("~")
 desktop = os.path.join(user_home, "Desktop")
@@ -18,3 +19,21 @@ def find_file_in_folder(base_folder, target_name):
             ):
                 return os.path.join(root, file)
     return None
+
+
+def silent_install_patch(patch_path):
+    try:
+        cmd = [patch_path, "/quiet", "/norestart"]
+        result = subprocess.run(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+
+        if result.returncode == 0:
+            print(f"설치 성공 : {patch_path}")
+            return True
+        else:
+            print(f"설치 실패 : {patch_path}")
+            return False
+    except Exception as e:
+        print(f"패치 설치 중 오류 발생 : {e}")
+        return False
