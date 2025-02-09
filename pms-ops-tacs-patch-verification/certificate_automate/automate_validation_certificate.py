37a959a0bb0f5282660053852474c95fc5f78ec0
nohheeyeon <heeyeon1213@gmail.com>
2025-02-09 18:53:15 +0900

    feat : 엑셀 데이터와 패치 파일 매핑
    - 패치 파일명을 엑셀 데이터의 제목과 매핑하여 QA 검증서에 반영

--- pms-ops-tacs-patch-verification/certificate_automate/automate_validation_certificate.py
+++ pms-ops-tacs-patch-verification/certificate_automate/automate_validation_certificate.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 
+import pandas as pd
 import win32com.client as win32
 
 
@@ -35,3 +36,27 @@ def create_hwp_file_with_v1_content(folder_name, excel_file):
 
     print(f"'{hwp_file_name}' 파일이 생성되고 내용이 추가되었습니다.")
     return hwp_file_name
+
+
+def map_patch_titles(file_list, excel_file):
+    df = pd.read_excel(excel_file)
+
+    patch_title_map = dict(zip(df["패치파일"], df["제목"]))
+
+    mapped_files = []
+    for file in file_list:
+        title = patch_title_map.get(file, None)
+        if title:
+            mapped_files.append(f"{title}\n{file}")
+        else:
+            mapped_files.append(file)
+
+    return mapped_files
+
+
+folder_name = "pms_patch/pms_patch"
+data_dir = os.path.dirname(os.path.abspath(__file__))
+downloads_path = os.path.join(data_dir, "data")
+excel_file = os.path.join(downloads_path, "ms_patch_list.xlsx")
+
+create_hwp_file_with_v1_content(folder_name, excel_file)
