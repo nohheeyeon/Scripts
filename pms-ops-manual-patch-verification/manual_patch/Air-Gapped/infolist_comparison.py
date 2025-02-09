c19e28adb68f9ea503b4fea02c4cca78299df893
nohheeyeon <heeyeon1213@gmail.com>
2025-02-09 14:45:05 +0900

    feat : patches.zip 내부 zip 파일 이름만 기록 기능 추가
    - patches.zip 내부에 포함된 또 다른 zip 파일까지는 비교가 불필요
    - process_zip 함수에서 patches.zip 내부 zip 파일은 zip 파일명만 출력

--- pms-ops-manual-patch-verification/manual_patch/Air-Gapped/infolist_comparison.py
+++ pms-ops-manual-patch-verification/manual_patch/Air-Gapped/infolist_comparison.py
@@ -22,27 +22,22 @@ BASE_DIRECTORY = f"C:/ftp_root/manual/ms/{year}/{month}"
 SW_DIRECTORY = f"C:/ftp_root/manual/sw/{year}/{month}"
 local_output_txt_path = os.path.join(BASE_DIRECTORY, "local_file_list.txt")
 remote_output_txt_path = os.path.join(BASE_DIRECTORY, "remote_file_list.txt")
-remote_modified_output_txt_path = os.path.join(
-    BASE_DIRECTORY, "remote_modified_file_list.txt"
-)
-log_file_path = os.path.join(BASE_DIRECTORY, "log.txt")
-
-with open(log_file_path, "w", encoding="utf-8") as log_file:
-    log_file.write("로그 파일 시작\n")
 
 
 def log(message):
-    timestamped_message = (
-        f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {message}"
-    )
-    print(timestamped_message)
-    with open(log_file_path, "a", encoding="utf-8") as log_file:
-        log_file.write(timestamped_message + "\n")
+    print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {message}")
 
 
 def exit_with_error(message):
-    log(message)
+    print(message)
     exit(1)
+
+
+def find_patches_directory(base_dir):
+    for root, dirs, _ in os.walk(base_dir):
+        if "patches" in dirs:
+            return os.path.join(root, "patches")
+    return None
 
 
 def process_zip(zip_file, parent_path=""):
@@ -53,12 +48,12 @@ def process_zip(zip_file, parent_path=""):
             else file_info.filename
         )
 
-        if "patches.zip" in current_path:
-            relative_path = current_path.split("patches.zip", 1)[1].lstrip("\\/")
-            all_file_names.add(relative_path)
+        if "patches" in current_path:
+            relative_path = current_path.split("patches", 1)[1].lstrip("\\/")
+            all_file_names.append(relative_path)
             log(f"파일 발견: {relative_path}")
         else:
-            all_file_names.add(current_path)
+            all_file_names.append(current_path)
             log(f"파일 발견: {current_path}")
 
         if file_info.filename.endswith(".zip") and "patches.zip" not in current_path:
@@ -71,16 +66,16 @@ def process_zip(zip_file, parent_path=""):
                     process_zip(nested_zip, parent_path=current_path)
 
 
-def process_top_level_zips(directory_path):
-    for file_name in os.listdir(directory_path):
-        if file_name.endswith(".zip"):
-            zip_path = os.path.join(directory_path, file_name)
-            log(f"최상위 Zip 파일 처리 중: {zip_path}")
-            try:
-                with zipfile.ZipFile(zip_path, "r") as zip_file:
-                    process_zip(zip_file, parent_path="")
-            except zipfile.BadZipFile:
-                log(f"잘못된 Zip 파일: {zip_path} - zip 형식이 아님")
+def process_directory(directory_path):
+
+log("SW 디렉토리 파일 및 patches.zip 내부 목록 작성 중")
+process_directory(SW_DIRECTORY)
+
+log(f"로컬 파일 목록 저장 중: {local_output_txt_path}")
+with open(local_output_txt_path, "w", encoding="utf-8") as output_file:
+    for file_name in all_file_names:
+        output_file.write(file_name + "\n")
+log("로컬 파일 목록 작성 완료")
 
 
 def fetch_remote_files(ssh_server, port, username, password, remote_dir):
@@ -92,24 +87,10 @@ def fetch_remote_files(ssh_server, port, username, password, remote_dir):
         stdin, stdout, stderr = ssh.exec_command(
             f"find {remote_dir} -type f -o -type d"
         )
-        all_files = set(
-            line.strip().replace(f"{remote_dir}/", "")
-            for line in stdout.read().decode("utf-8").splitlines()
-        )
+        remote_files = stdout.read().decode("utf-8").splitlines()
 
-        log("원격 서버의 v1 하위 경로부터 모든 파일 경로 목록 추출 완료")
-
-        month_filter = now.strftime("%Y-%m")
-        stdin, stdout, stderr = ssh.exec_command(
-            f"find {remote_dir} -type f -newermt {month_filter}-01 ! -newermt {month_filter}-31"
-        )
-        modified_files = set(
-            line.strip().replace(f"{remote_dir}/", "")
-            for line in stdout.read().decode("utf-8").splitlines()
-        )
-
-        log("원격 서버에서 이번 달에 수정된 파일 목록 추출 완료")
-        return all_files, modified_files
+        log("원격 서버의 파일/폴더 목록 추출 완료")
+        return [file.replace(f"{remote_dir}/", "") for file in remote_files]
     except Exception as e:
         exit_with_error(f"원격 서버에서 파일/폴더 목록을 가져오는 데 실패: {str(e)}")
     finally:
@@ -125,13 +106,41 @@ REMOTE_DIRECTORY = ""
 remote_files = fetch_remote_files(
     SSH_SERVER, PORT, USERNAME, PASSWORD, REMOTE_DIRECTORY
 )
-log(f"중복 제거된 원격 서버 파일 목록 저장 중: {remote_output_txt_path}")
+log(f"원격 서버 파일 목록 저장 중: {remote_output_txt_path}")
 with open(remote_output_txt_path, "w", encoding="utf-8") as output_file:
-    for file in sorted(remote_files):
+    for file in remote_files:
         output_file.write(file + "\n")
-log("원격 서버의 v1 하위 경로부터 전체 파일 목록 작성 완료")
+log("원격 서버 파일 목록 작성 완료")
 
-with open(remote_modified_output_txt_path, "w", encoding="utf-8") as output_file:
-    for file in sorted(modified_files):
-        output_file.write(file + "\n")
-log("이번 달에 수정된 원격 서버 파일 목록 작성 완료")
+remote_files_set = set(remote_files)
+local_only_files = [file for file in all_file_names if file not in remote_files_set]
+log("로컬 디렉토리에만 있는 파일:")
+for file in local_only_files:
+    print(file)
+
+local_files_set = set(all_file_names)
+remote_only_files = [file for file in remote_files if file not in local_files_set]
+log("원격 서버에만 있는 파일:")
+for file in remote_only_files:
+    print(file)
+
+no_ayt_files = []
+log("동일한 이름의 .ayt 파일이 존재하지 않는 파일:")
+for file in all_file_names:
+    if file.startswith("ms_files/") and file.split(".")[-1] in {"cab", "exe"}:
+        ayt_file_path = os.path.join(LOCAL_DIRECTORY, f"{file}.ayt")
+        if not os.path.isfile(ayt_file_path):
+            print(file)
+            no_ayt_files.append(file)
+
+if not no_ayt_files:
+    log("모든 파일에 대해 동일한 이름의 .ayt 파일이 존재합니다.")
+
+if not no_ayt_files:
+    log("스크립트 실행 성공")
+    exit_code = 0
+else:
+    log("스크립트 실행 실패")
+    exit_code = 1
+
+exit(exit_code)
